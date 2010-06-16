57a01f31ea2813c4a5d6d294c9e3d87fd97f9d9c
Daniel Roberts <Ademan555@gmail.com>
2010-06-16 00:23:07 -0700

    Completed PyArray_Scalar() modifications, still needs testing.  ucsnarrow is now used unconditionally to assist PyArray_Scalar() handling Unicode objects.

--- numpy/core/src/multiarray/ucsnarrow.h
+++ numpy/core/src/multiarray/ucsnarrow.h
@@ -1,11 +1,6 @@
 #ifndef _NPY_UCSNARROW_H_
 #define _NPY_UCSNARROW_H_
 
-#ifdef Py_UNICODE_WIDE
-#error this should not be included if Py_UNICODE_WIDE is defined
-int int int;
-#endif
-
 NPY_NO_EXPORT int
 PyUCS2Buffer_FromUCS4(Py_UNICODE *ucs2, PyArray_UCS4 *ucs4, int ucs4length);
 
@@ -13,9 +8,6 @@ NPY_NO_EXPORT int
 PyUCS2Buffer_AsUCS4(Py_UNICODE *ucs2, PyArray_UCS4 *ucs4, int ucs2len, int ucs4len);
 
 NPY_NO_EXPORT PyObject *
-MyPyUnicode_New(int length);
-
-NPY_NO_EXPORT int
-MyPyUnicode_Resize(PyUnicodeObject *uni, int length);
+MyPyUnicode_New(int length); /* XXX: find callsites and see what length is (bytes, 16 bit words, what) */
 
 #endif
